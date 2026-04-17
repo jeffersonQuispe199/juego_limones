761a69e7d4669b3d0ef2a921b2411df491ec04e5
JEFFERON QUISPE <jeffersonquispecando@gmail.com>
2026-04-17 08:21:44 -0500

    mover  a la izquierda

--- limones.js
+++ limones.js
@@ -1,9 +1,15 @@
 let canvas =document.getElementById("areaJuego");
 let ctx=canvas.getContext("2d");
 
+
+
 const ALTURA_SUELO=40;
 const ALTURA_PERSONAJE= 60;
 const ANCHO_PERSONAJE=40;
+
+let personajeX=canvas.width/2;
+//let personajeY=0;
+
 function iniciar(){ 
     dibujarSuelo();
     dibujarPersonaje();
@@ -16,5 +22,20 @@ function dibujarSuelo(){
 }
 function dibujarPersonaje(){
     ctx.fillStyle="yellow";
-    ctx.fillRect(canvas.width/2,canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE),ANCHO_PERSONAJE,ALTURA_PERSONAJE);
+    ctx.fillRect(personajeX,canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE),ANCHO_PERSONAJE,ALTURA_PERSONAJE);
+}
+function moverIzquierda(){
+    personajeX=personajeX-10;
+    actualizarPantalla();
+    
+}
+function limpiarCanva(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+
+}
+function actualizarPantalla(){
+    limpiarCanva();
+    dibujarSuelo();
+    dibujarPersonaje();
+
 }
