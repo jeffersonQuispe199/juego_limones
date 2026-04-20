c36a6091ea9dfccd995616c392b1ac508c56b09c
JEFFERON QUISPE <jeffersonquispecando@gmail.com>
2026-04-19 20:23:16 -0500

    TALLER JUEGOS LIMONES AJUSTES RETO4

--- limones.js
+++ limones.js
@@ -12,6 +12,9 @@ let limonY=0;
 
 let puntaje=0;
 let vidas=3;
+let velocidadCaida=200;
+let intervalo;
+
 
 
 let personajeX=canvas.width/2;
@@ -20,6 +23,7 @@ let personajeY=canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE);
 
 
 function iniciar(){ 
+    intervalo=setInterval(bajarLimon,velocidadCaida);
     dibujarSuelo();
     dibujarPersonaje();
   aparecerLimon();
@@ -77,27 +81,72 @@ function detectarAtrapado(){
         && limonY+ANCHO_LIMON > personajeY &&
          limonY < personajeY+ALTURA_PERSONAJE){
        // alert("ATRAPADO");
-       aparecerLimon();
+      
        puntaje=puntaje+1;
-       mostraEnSpan("txtPuntaje",puntaje)
 
+       mostraEnSpan("txtPuntaje",puntaje);
+       if (puntaje === 3) {
+            cambiarVelocidad(150);
+        } else if (puntaje === 6) {
+            cambiarVelocidad(100);
+        } else if (puntaje === 10) {
+            alert("¡GANASTE! BRO tienes los limones, ahora falta la sal y el tequila 🍋"); 
+            finalizarJuego();
+        }
+
+        if (puntaje < 10) {
+            aparecerLimon();
+        }
     }
 }
+
+function cambiarVelocidad(nuevaVelocidad) {
+    clearInterval(intervalo);
+    velocidadCaida = nuevaVelocidad;
+    intervalo = setInterval(bajarLimon, velocidadCaida);
+}
+
+    
 function detectarPiso(){
     if (limonY+ALTURA_LIMON==canvas.height-ALTURA_SUELO){
         aparecerLimon();
         vidas=vidas-1;
         mostraEnSpan("txtVidas",vidas);
+        if (vidas === 0){
+            alert("GAME OVER");
+            finalizarJuego();
+
+        }else{
+            aparecerLimon();
+
+        }
     }
 }
 
-
+function finalizarJuego(){
+    clearInterval(intervalo);
+}
 function aparecerLimon(){
     limonX=generarAleatorio(0,canvas.width-ALTURA_LIMON);
     limonY=0;
     actualizarPantalla();
 }
 
+function reiniciar() {
+ 
+    vidas = 3;
+    puntaje = 0;
+    velocidadCaida = 200;
+    personajeX = canvas.width / 2;
+    
+   
+    mostraEnSpan("txtPuntaje", puntaje);
+    mostraEnSpan("txtVidas", vidas);
+    
+    
+    clearInterval(intervalo);
+    iniciar();
+}
 
 
  
